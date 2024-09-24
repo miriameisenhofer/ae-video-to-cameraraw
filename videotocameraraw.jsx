ba34d2efe50b78a3445e93ecb355ccd08e7b61e0
Miriam Eisenhofer <miriam.eisenhofer@rwth-aachen.de>
2024-09-24 16:26:52 +0200

    Restructure: Remove png support

--- videotocameraraw.jsx
+++ videotocameraraw.jsx
@@ -2,7 +2,7 @@ var window = new Window("palette", "Video to CameraRaw", undefined);
 window.orientation = "column";
 
 // Get dimensions of source footage
-var footage = app.project.item(1);
+var footage = app.project.item(2);
 var footageName = footage.name;
 var footageWidth = footage.width;
 var footageHeight = footage.height;
@@ -10,35 +10,29 @@ var footagePAR = footage.pixelAspect;
 var footageDuration = footage.duration;
 var footageFPS = footage.frameRate;
 
+app.beginUndoGroup("Video to Camera Raw");
 // Create comp of source footage
 var comp = app.project.items.addComp(footageName + "_VTCR", footageWidth, footageHeight, footagePAR, footageDuration, footageFPS);
+comp.layers.add(footage);
 
-// Render source footage to png/psd sequence (drop down menu)
-var fileTypeGroup = window.add("group", undefined, "");
-fileTypeGroup.orientation = "row";
-var fileTypes = [".psd", ".png"];
-fileTypeGroup.add("statictext", undefined, "File Type: ")
-var dropDownFileType = fileTypeGroup.add("dropdownlist", undefined, fileTypes);
-dropDownFileType.selection = 0;
+// Render source footage to psd sequence
+var outputFolder = "";
+if (app.project.file == null) {
+    alert("Please save project");
+} else {
+    var renderQueueItem = app.project.renderQueue.items.add(comp);
+    setRenderSettings(renderQueueItem, 1);
+
+    // Import psd sequence as camera raw files
+}
+app.endUndoGroup();
 
 window.center();
 window.show();
 
-var renderQueueItem = app.project.renderQueue.items.add(comp);
-setRenderSettings(dropDownFileType, renderQueueItem, 1);
-
-// Import png/psd sequence as camera raw files
-
 
 // Functions
 
-function setRenderSettings(dropDown, rqItem, rqIndex) {
-    if (dropDown.selection.text == ".psd") {
-        rqItem.outputModule(rqIndex).applyTemplate("Photoshop");
-    } else if (dropDown.selection.text == ".png") {
-        rqItem.outputModule(rqIndex).applyTemplate("PNG");
-    } else {
-        // Default: .psd
-        rqItem.outputModule(rqIndex).applyTemplate("Photoshop");
-    }
+function setRenderSettings(rqItem, rqIndex) {
+    rqItem.outputModule(rqIndex).applyTemplate("Photoshop");
 }
