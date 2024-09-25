dde9d73fb77c5ccaae9d7b3ee4ecaa0f3171029d
Miriam Eisenhofer <miriam.eisenhofer@rwth-aachen.de>
2024-09-25 12:03:31 +0200

    Execute .psd sequence render into subfolder /VCTR_renders/filename/filename_[#####].psd

--- videotocameraraw.jsx
+++ videotocameraraw.jsx
@@ -2,7 +2,7 @@ var window = new Window("palette", "Video to CameraRaw", undefined);
 window.orientation = "column";
 
 // Get dimensions of source footage
-var footage = app.project.item(2);
+var footage = app.project.item(1);
 var footageName = footage.name;
 var footageWidth = footage.width;
 var footageHeight = footage.height;
@@ -10,22 +10,36 @@ var footagePAR = footage.pixelAspect;
 var footageDuration = footage.duration;
 var footageFPS = footage.frameRate;
 
-app.beginUndoGroup("Video to Camera Raw");
+app.beginUndoGroup("Video to Camera Raw (Pre-render)");
 // Create comp of source footage
 var comp = app.project.items.addComp(footageName + "_VTCR", footageWidth, footageHeight, footagePAR, footageDuration, footageFPS);
 comp.layers.add(footage);
 
 // Render source footage to psd sequence
-var outputFolder = "";
 if (app.project.file == null) {
     alert("Please save project");
 } else {
+    var outputFolder = new Folder(app.project.file.parent.fsName + "/VTCR_renders/" + comp.name.substring(0, comp.name.lastIndexOf(".")));
     var renderQueueItem = app.project.renderQueue.items.add(comp);
-    setRenderSettings(renderQueueItem, 1);
+    // Set render settings
+    var outputModule = renderQueueItem.outputModule(1);
+    outputModule.applyTemplate("Photoshop");
+    //setRenderSettings(renderQueueItem, 1);
+    if (!outputFolder.exists) {
+        if (!outputFolder.create()) {
+            alert("Couldn't create render folder");
+        }
+    }
+    if (outputFolder.exists){
+        outputModule.file = File(outputFolder.fsName + "/" + comp.name + "_VTCR");
+        app.endUndoGroup();
+        app.project.renderQueue.render();
 
-    // Import psd sequence as camera raw files
+        app.beginUndoGroup("Video to Camera Raw (Post-render)");
+        // Import psd sequence as camera raw files
+        app.endUndoGroup();
+    }
 }
-app.endUndoGroup();
 
 window.center();
 window.show();
