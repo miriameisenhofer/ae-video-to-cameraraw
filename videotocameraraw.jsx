6e098ca1ca976198b585bca38c817e8e4c6cc9bc
Miriam Eisenhofer <miriam.eisenhofer@rwth-aachen.de>
2024-09-25 12:24:48 +0200

    Use selected footage item

--- videotocameraraw.jsx
+++ videotocameraraw.jsx
@@ -1,48 +1,55 @@
 var window = new Window("palette", "Video to CameraRaw", undefined);
 window.orientation = "column";
 
-// Get dimensions of source footage
-var footage = app.project.item(1);
-var footageName = footage.name;
-var footageWidth = footage.width;
-var footageHeight = footage.height;
-var footagePAR = footage.pixelAspect;
-var footageDuration = footage.duration;
-var footageFPS = footage.frameRate;
-
-app.beginUndoGroup("Video to Camera Raw (Pre-render)");
-// Create comp of source footage
-var comp = app.project.items.addComp(footageName + "_VTCR", footageWidth, footageHeight, footagePAR, footageDuration, footageFPS);
-comp.layers.add(footage);
-
-// Render source footage to psd sequence
-if (app.project.file == null) {
+// Ensure active item is a footage item
+if ((app.project.activeItem == null) || !(app.project.activeItem instanceof FootageItem)) {
+    alert("Select a footage item!");
+// Ensure project is saved
+} else if (app.project.file == null) {
     alert("Please save project");
 } else {
-    var outputFolder = new Folder(app.project.file.parent.fsName + "/VTCR_renders/" + comp.name.substring(0, comp.name.lastIndexOf(".")));
-    var renderQueueItem = app.project.renderQueue.items.add(comp);
-    // Set render settings
-    var outputModule = renderQueueItem.outputModule(1);
-    outputModule.applyTemplate("Photoshop");
-    //setRenderSettings(renderQueueItem, 1);
-    if (!outputFolder.exists) {
-        if (!outputFolder.create()) {
-            alert("Couldn't create render folder");
+    // Get dimensions of source footage
+    var footage = app.project.activeItem;
+    var footageName = footage.name;
+    var footageWidth = footage.width;
+    var footageHeight = footage.height;
+    var footagePAR = footage.pixelAspect;
+    var footageDuration = footage.duration;
+    var footageFPS = footage.frameRate;
+    
+    app.beginUndoGroup("Video to Camera Raw (Pre-render)");
+    // Create comp of source footage
+    var comp = app.project.items.addComp(footageName + "_VTCR", footageWidth, footageHeight, footagePAR, footageDuration, footageFPS);
+    comp.layers.add(footage);
+
+    // Render source footage to psd sequence
+    if (app.project.file == null) {
+        alert("Please save project");
+    } else {
+        var outputFolder = new Folder(app.project.file.parent.fsName + "/VTCR_renders/" + comp.name.substring(0, comp.name.lastIndexOf(".")));
+        var renderQueueItem = app.project.renderQueue.items.add(comp);
+        // Set render settings
+        var outputModule = renderQueueItem.outputModule(1);
+        outputModule.applyTemplate("Photoshop");
+        //setRenderSettings(renderQueueItem, 1);
+        if (!outputFolder.exists) {
+            if (!outputFolder.create()) {
+                alert("Couldn't create render folder");
+            }
+        }
+        if (outputFolder.exists){
+            outputModule.file = File(outputFolder.fsName + "/" + comp.name + "_VTCR");
+            app.endUndoGroup();
+            //app.project.renderQueue.render();
+
+            app.beginUndoGroup("Video to Camera Raw (Post-render)");
+            // Import psd sequence as camera raw files
+            app.endUndoGroup();
         }
     }
-    if (outputFolder.exists){
-        outputModule.file = File(outputFolder.fsName + "/" + comp.name + "_VTCR");
-        app.endUndoGroup();
-        app.project.renderQueue.render();
-
-        app.beginUndoGroup("Video to Camera Raw (Post-render)");
-        // Import psd sequence as camera raw files
-        app.endUndoGroup();
-    }
+    window.center();
+    window.show();
 }
-
-window.center();
-window.show();
 
 
 // Functions
